788e40b0b534b8a26e00baf8a345da31aafb43fc
TML45 <tassio.melo45@gmail.com>
2024-07-15 00:14:37 -0300

    feat: Add exception treatment

--- src/main/java/com/e3gsix/fiap/tech_challenge_4_customer_management/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/e3gsix/fiap/tech_challenge_4_customer_management/service/impl/CustomerServiceImpl.java
@@ -1,5 +1,6 @@
 package com.e3gsix.fiap.tech_challenge_4_customer_management.service.impl;
 
+import com.e3gsix.fiap.tech_challenge_4_customer_management.controller.exception.NotFoundException;
 import com.e3gsix.fiap.tech_challenge_4_customer_management.model.Customer;
 import com.e3gsix.fiap.tech_challenge_4_customer_management.repository.CustomerRepository;
 import com.e3gsix.fiap.tech_challenge_4_customer_management.service.CustomerService;
@@ -17,36 +18,34 @@ public class CustomerServiceImpl implements CustomerService {
     private final CustomerRepository customerRepository;
 
     @Override
-    public Customer findById(Long customerId) {
-        return customerRepository.findById(customerId).orElse(null);
-    }
-
-    @Override
     public ResponseEntity<?> create(Customer customer) {
-
         return ResponseEntity.ok(customerRepository.save(customer));
     }
 
     @Override
-    public Customer update(Long customerId, Customer customer) {
-        Customer customerExist = customerRepository.findById(customerId).orElse(null);
+    public Customer findById(Long customerId) {
+        return customerRepository.findById(customerId).orElseThrow(() -> createNotFound(customerId));
+    }
 
-        if (customerExist != null) {
-            customerExist.setName(customer.getName());
-            customerExist.setUf(customer.getUf());
-            customerExist.setAddress(customer.getAddress());
-            customerRepository.save(customerExist);
-        }
+
+    @Override
+    public Customer update(Long customerId, Customer customer) {
+        Customer customerExist = customerRepository.findById(customerId).orElseThrow(() -> createNotFound(customerId));
+        customerExist.setName(customer.getName());
+        customerExist.setUf(customer.getUf());
+        customerExist.setAddress(customer.getAddress());
+        customerRepository.save(customerExist);
         return customerExist;
     }
 
     @Override
     public Customer delete(Long customerId) {
-        Customer customerExist = customerRepository.findById(customerId).orElse(null);
-
-        if (customerExist != null) {
-            customerRepository.delete(customerExist);
-        }
+        Customer customerExist = customerRepository.findById(customerId).orElseThrow(() -> createNotFound(customerId));
+        customerRepository.delete(customerExist);
         return customerExist;
+    }
+
+    private NotFoundException createNotFound(Long customerId) {
+        return new NotFoundException(String.format("Customer with ID '%s' not found.", customerId));
     }
 }
